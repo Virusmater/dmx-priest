a1d6abebc0229984e62be2d314810045e6044e7a
Virusmater <virusmater@gmail.com>
2020-07-22 00:23:21 +0200

    change the address

--- main.py
+++ main.py
@@ -6,26 +6,9 @@ MAIN_MENU = 0
 PLAY_MENU = 1
 RECORD_MENU = 2
 
-lcd = RPi_I2C_driver.lcd()
-lcd.lcd_display_string("Play mode", 1)
-lcd.lcd_display_string("press knob...", 2)
-
 
 def get_preset_name(position):
     return glob.glob("presets/*")[position]
-
-
-def set_text(menu, position):
-    if menu == MAIN_MENU:
-        if position <= 20:
-            lcd.lcd_display_string("Play mode  ", 1)
-            lcd.lcd_display_string("push knob...", 2)
-        elif position > 20:
-            lcd.lcd_display_string("Record mode", 1)
-            lcd.lcd_display_string("push knob...", 2)
-    elif menu == PLAY_MENU:
-        lcd.lcd_display_string("Select and push:", 1)
-        lcd.lcd_display_string(get_preset_name(position), 2)
 
 
 class Menu:
@@ -34,6 +17,20 @@ class Menu:
         self.position = 0
         self.menu = MAIN_MENU
         self.rotary = rotary_encoder.RotaryEncoder()
+        self.lcd = RPi_I2C_driver.lcd()
+        self.set_text()
+
+    def set_text(self):
+        if self.menu == MAIN_MENU:
+            if self.position <= 20:
+                self.lcd.lcd_display_string("Play mode  ", 1)
+                self.lcd.lcd_display_string("push knob...", 2)
+            elif self.position > 20:
+                self.lcd.lcd_display_string("Record mode", 1)
+                self.lcd.lcd_display_string("push knob...", 2)
+        elif self.menu == PLAY_MENU:
+            self.lcd.lcd_display_string("Select and push:", 1)
+            self.lcd.lcd_display_string(get_preset_name(self.position), 2)
 
     def select(self):
         if self.menu == MAIN_MENU:
