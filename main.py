a0cf696ddcd1b5726b0417bf66b6866a3a29a52e
Virusmater <virusmater@gmail.com>
2020-07-21 23:50:31 +0200

    change the address

--- main.py
+++ main.py
@@ -4,7 +4,12 @@ lcd = RPi_I2C_driver.lcd()
 lcd.lcd_display_string("Play mode", 1)
 lcd.lcd_display_string("press knob...", 2)
 rotary = rotary_encoder.RotaryEncoder()
+position = 0
 
 while True:
     message = rotary.eventq.get()
-    print(message)
+    if message == rotary.LEFT or message == rotary.RIGHT:
+        position += message
+        print(position)
+    else:
+        print("button")
