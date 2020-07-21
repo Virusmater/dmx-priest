899a6c9325d90f9ad2bdef88a7ededeb7623e79f
Virusmater <virusmater@gmail.com>
2020-07-21 23:46:41 +0200

    change the address

--- lib/rotary_encoder.py
+++ lib/rotary_encoder.py
@@ -1,33 +1,33 @@
 from gpiozero import Button
 import queue
 
-eventq = queue.Queue()
 
-pin_a = Button(18)  # Rotary encoder pin A connected to GPIO2
-pin_b = Button(27)  # Rotary encoder pin B connected to GPIO3
-sw = Button(17)
+class RotaryEncoder:
 
-LEFT = -1
-RIGHT = 1
-BUTTON = 0
+    LEFT = -1
+    RIGHT = 1
+    BUTTON = 0
 
+    def __init__(self):
+        self.eventq = queue.Queue()
 
-def pin_a_rising():  # Pin A event handler
-    if pin_b.is_pressed:
-        eventq.put(LEFT)  # pin A rising while A is active is a clockwise turn
+        self.pin_a = Button(18)  # Rotary encoder pin A connected to GPIO2
+        self.pin_b = Button(27)  # Rotary encoder pin B connected to GPIO3
+        self.sw = Button(17)
+        self.pin_a.when_pressed = self.pin_a_rising  # Register the event handler for pin A
+        self.pin_b.when_pressed = self.pin_b_rising  # Register the event handler for pin B
+        self.selfsw.when_pressed = self.pin_sw_rising
 
+    def pin_a_rising(self):  # Pin A event handler
+        if self.pin_b.is_pressed:
+            self.eventq.put(RotaryEncoder.LEFT)  # pin A rising while A is active is a clockwise turn
 
-def pin_b_rising():  # Pin B event handler
-    if pin_a.is_pressed:
-        eventq.put(RIGHT)  # pin B rising while A is active is a clockwise turn
+    def pin_b_rising(self):  # Pin B event handler
+        if self.pin_a.is_pressed:
+            self.eventq.put(RotaryEncoder.RIGHT)  # pin B rising while A is active is a clockwise turn
 
+    def pin_sw_rising(self):
+        if self.sw.is_pressed:
+            self.eventq.put(RotaryEncoder.BUTTON)
 
-def pin_sw_rising():
-    if sw.is_pressed:
-        eventq.put(BUTTON)
-
-
-pin_a.when_pressed = pin_a_rising  # Register the event handler for pin A
-pin_b.when_pressed = pin_b_rising  # Register the event handler for pin B
-sw.when_pressed = pin_sw_rising
 
--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from lib import RPi_I2C_driver, rotary_encoder
 lcd = RPi_I2C_driver.lcd()
 lcd.lcd_display_string("Play mode", 1)
 lcd.lcd_display_string("press knob...", 2)
+rotary = rotary_encoder()
 
 while True:
-    rotary = rotary_encoder()
     message = rotary.eventq.get()
     print(message)
