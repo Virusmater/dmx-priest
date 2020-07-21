0b78b169da40e9a4781027f9c78419632ea4f1e6
Virusmater <virusmater@gmail.com>
2020-07-21 23:56:29 +0200

    change the address

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 from lib import RPi_I2C_driver, rotary_encoder
 
+MAIN_MENU = 0
+PLAY_MENU = 1
+RECORD_MENU = 2
+
 lcd = RPi_I2C_driver.lcd()
 lcd.lcd_display_string("Play mode", 1)
 lcd.lcd_display_string("press knob...", 2)
 rotary = rotary_encoder.RotaryEncoder()
 position = 0
+menu = MAIN_MENU
+
+
+def set_text(menu, position):
+    if menu == MAIN_MENU:
+        if position <= 20:
+            lcd.lcd_display_string("Play mode", 1)
+            lcd.lcd_display_string("press knob...", 2)
+        elif position > 20:
+            lcd.lcd_display_string("Record mode", 1)
+            lcd.lcd_display_string("press knob...", 2)
+
 
 while True:
     message = rotary.eventq.get()
@@ -13,3 +29,4 @@ while True:
         print(position)
     else:
         print("button")
+    set_text(menu, position)
