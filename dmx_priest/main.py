83db62548911e19554e7cc8b22de0043ee9be3ad
Virusmater <virusmater@gmail.com>
2026-02-02 20:45:41 +0100

    Fix lcd_address conversion to handle default base

--- dmx_priest/main.py
+++ dmx_priest/main.py
@@ -193,7 +193,7 @@ def main():
     print({section: dict(config[section]) for section in config})
     beamer = Beamer(device=config['DEFAULT']['beamer_device'])
 
-    menu = Menu(beamer = beamer, lcd_address = int(config['DEFAULT']['lcd_address']))
+    menu = Menu(beamer = beamer, lcd_address = int(config['DEFAULT']['lcd_address'], 0))
     pool()
 
 
