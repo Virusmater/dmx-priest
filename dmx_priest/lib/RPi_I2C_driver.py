91935e1a43d241502ef65e7c061543efc1de2b74
Virusmater <virusmater@gmail.com>
2026-02-02 20:40:08 +0100

    Update RPi_I2C_driver.py

--- dmx_priest/lib/RPi_I2C_driver.py
+++ dmx_priest/lib/RPi_I2C_driver.py
@@ -102,8 +102,8 @@ Rs = 0b00000001  # Register select bit
 
 class lcd:
     # initializes objects and lcd
-    def __init__(self):
-        self.lcd_device = i2c_device(ADDRESS)
+    def __init__(self, address):
+        self.lcd_device = i2c_device(address)
 
         self.lcd_write(0x03)
         self.lcd_write(0x03)
